cf286e2b3eb9d55841ddbfa7beae5f2d2da91081
Andrew <andrew.larse514@gmail.com>
2016-01-30 01:58:30 -0600

    initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class main {
+	
+	public static void main(String [] args)
+	{
+		String testString = "You can't not be fillin this";
+		
+		System.out.println(testString);
+	}
+}
